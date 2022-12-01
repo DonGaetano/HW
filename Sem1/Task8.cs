1a2e1701ed6f75bcc0d559175557fe827ad3bac8
Nikita <nsimonovbizzz@gmail.com>
2022-12-01 18:57:50 +0300

    4 Tasks

---
+++ Sem1/Task8.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N");
+
+string num1 = Console.ReadLine()!;
+int parsed1 = int.Parse(num1);
+
+int count = 0;
+int number = 1;
+number = number++;
+
+while (number <= parsed1)
+{
+    for (int i = 1; i % 2 == 0; i++)
+    {
+        count = count++;
+    }
+}
+Console.WriteLine("От 1 до " + parsed1 + " всего " + count + " четных чисел");
+
+
